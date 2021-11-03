fd895665c29eafd8965aed527e1e9ff689dd36d2
Jathevan <jathevan15@gmail.com>
2021-11-03 12:14:36 +0530

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task1API.Models
+{
+    public class User
+    {
+     /*   public long Id { get; set; }*/
+        public string name { get; set; }
+        public string phone { get; set; }
+
+        public User(string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
+
+        }
+    }
+}
