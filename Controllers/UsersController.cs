5679f2f8ea3f307014683da842510ce2a0e5d8af
Jathevan <jathevan15@gmail.com>
2021-11-08 22:36:28 +0530

    Clean the code

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using task1API.Models;
 
 namespace task1API.Controllers
@@ -26,93 +21,7 @@ namespace task1API.Controllers
         {
             List<User> users = new List<User>();
             users.Add(new User("test", "test"));
-             return users;
+            return users;
         }
-        /* public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-         {
-             return await _context.Users.ToListAsync();
-         }*/
-
-        // GET: api/Users/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(long id)
-        {
-            var user = await _context.Users.FindAsync(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return user;
-        }
-
-      /*  // PUT: api/Users/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(long id, User user)
-        {
-            if (id != user.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(user).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }*/
-
-     /*   // POST: api/Users
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<User>> PostUser(User user)
-        {
-            User user1 = new User( "test", "test");
-          //  user.Add(new User("test", "test"));
-            _context.Users.Add(user);
-           // user1.Add(new User("test", "test"));
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetUser", new { id = user.Id }, user);
-        }*/
-
-        // DELETE: api/Users/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<User>> DeleteUser(long id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
-
-            return user;
-        }
-
-     /*   private bool UserExists(long id)
-        {
-            return _context.Users.Any(e => e.Id == id);
-        }*/
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace task1API.Models
+﻿namespace task1API.Models
 {
     public class User
     {
-     /*   public long Id { get; set; }*/
-        public string name { get; set; }
-        public string phone { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
 
         public User(string name, string phone)
         {
-            this.name = name;
-            this.phone = phone;
-
+            this.Name = name;
+            this.Phone = phone;
         }
     }
 }
